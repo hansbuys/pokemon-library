3195708958abe1a0d7671e30d626123aef0a2e98
Hans Buys <hans@integral.io>
2019-12-13 10:40:20 -0500

    Clean up generated code and add Header.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Contains a header', () => {
+    const {getByText} = render(<App/>);
+    const linkElement = getByText(/Pokemon Library/);
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveClass("header")
 });
---
+++ src/MainPageHeader.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Header() {
+    return <h1 className={"header"}>Pokemon Library</h1>
+}
