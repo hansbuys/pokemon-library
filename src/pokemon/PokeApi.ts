17dc0c9b8ce69fac000962ecdf63065ff90af935
Hans Buys <hans@integral.io>
2019-12-16 09:03:12 -0500

    Implement PokeApi with actual client call.

--- src/pokemon/PokeApi.ts
+++ src/pokemon/PokeApi.ts
@@ -1,13 +1,27 @@
 import {PokemonRepository} from "./PokemonRepository";
 import {Pokemon} from "./Pokemon";
+import * as restm from 'typed-rest-client/RestClient';
+
+type PokemonList = {
+    count: number
+    next: string
+    previous: string
+    results: {
+        name: string
+        url: string
+    }[]
+}
 
 export class PokeApi implements PokemonRepository {
-    // private readonly baseUrl: string = "https://pokeapi.co/api/v2/";
+    private readonly baseUrl: string = "https://pokeapi.co";
+    private readonly client = new restm.RestClient("", this.baseUrl);
+
     getAll(): Promise<Pokemon[]> {
-        return new Promise<Pokemon[]>((resolve) => {
-            resolve([{
-                name: "Bulbasaur"
-            }]);
+        return this.client.get<PokemonList>("/api/v2/pokemon").then(value => {
+            if (!value.result) {
+                return Promise.resolve([]);
+            }
+            return value.result.results.map((p) => ({name: p.name}));
         });
     }
 }
