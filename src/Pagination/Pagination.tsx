f7d1d08bc54c1cd529efb510b3e584d8733d6610
Hans Buys <hans@integral.io>
2020-01-12 18:11:23 -0500

    Can now navigate using arrow keys.

--- src/Pagination/Pagination.tsx
+++ src/Pagination/Pagination.tsx
@@ -1,46 +1,79 @@
-import React from "react";
+import React, {Component} from "react";
 import {Paginated} from "../PokemonRepository/PokemonRepository";
 import {Logging} from "../Logging";
 import './Pagination.css';
 
-export function Pagination(props: { pages: Paginated<any>, onPageChange: (newPage: number) => void }) {
-    const logger = Logging.createLogger(Pagination);
-    const numberOfPages = Math.ceil(props.pages.totalCount / props.pages.pageSize);
-    const maxNumberOfPages = 10;
+type PaginationProps = {
+    pages: Paginated<any>,
+    onPageChange: (newPage: number) => void
+}
 
-    function pageClick(pageNumber: number, currentPage: number) {
+export class Pagination extends Component<PaginationProps> {
+
+    private readonly logger = Logging.createLogger(Pagination);
+    private readonly maxNumberOfPages: number = 10;
+
+    constructor(props: PaginationProps) {
+        super(props);
+    }
+
+    componentDidMount(): void {
+        document.onkeydown = e => {
+            const currentPage = this.props.pages.offset + 1;
+            switch (e.key) {
+                case "ArrowLeft": // left
+                    this.pageClick(currentPage - 1);
+                    break;
+
+                case "ArrowRight": // right
+                    this.pageClick(currentPage + 1);
+                    break;
+
+                default:
+                    return; // exit this handler for other keys
+            }
+        };
+    }
+
+    pageClick(pageNumber: number): void {
+        const numberOfPages = Math.ceil(this.props.pages.totalCount / this.props.pages.pageSize);
+        const currentPage = this.props.pages.offset + 1;
         if (pageNumber !== currentPage && pageNumber !== 0 && pageNumber <= numberOfPages) {
-            props.onPageChange(pageNumber);
+            this.props.onPageChange(pageNumber);
         }
     }
 
-    if (props.pages.totalCount > props.pages.pageSize) {
-        const currentPage = props.pages.offset + 1;
-        logger.info(`Displaying page ${currentPage}/${numberOfPages}`);
+    render() {
+        const numberOfPages = Math.ceil(this.props.pages.totalCount / this.props.pages.pageSize);
+        const currentPage = this.props.pages.offset + 1;
+        this.logger.info(`Displaying page ${currentPage}/${numberOfPages}`);
 
-        const maxOffset = Math.max(numberOfPages - maxNumberOfPages + 1, 1);
-        const naiveOffset = Math.max(currentPage - Math.ceil(maxNumberOfPages / 2) + 1, 1);
-        logger.trace(`Offsets: Naive=${naiveOffset} Max=${maxOffset}`);
-        let offset = Math.min(maxOffset, naiveOffset);
-        const pages = Array.from(Array(Math.min(numberOfPages, maxNumberOfPages)).keys(), v => v + offset);
-        logger.trace(`Pages on page: ${pages.join(", ")}`);
+        if (this.props.pages.totalCount > this.props.pages.pageSize) {
+            const maxOffset = Math.max(numberOfPages - this.maxNumberOfPages + 1, 1);
+            const naiveOffset = Math.max(currentPage - Math.ceil(this.maxNumberOfPages / 2) + 1, 1);
+            this.logger.trace(`Offsets: Naive=${naiveOffset} Max=${maxOffset}`);
+            let offset = Math.min(maxOffset, naiveOffset);
+            const pages = Array.from(Array(Math.min(numberOfPages, this.maxNumberOfPages)).keys(), v => v + offset);
+            this.logger.trace(`Pages on page: ${pages.join(", ")}`);
 
-        return <div>
-            <ul className="pagination">
-                <li key="previous"
-                    className={currentPage === 1 ? "disable" : ""}
-                    onClick={() => pageClick(currentPage - 1, currentPage)}>&lt;</li>
-                {pages.map((i) =>
-                    <li key={i}
-                        className={i === currentPage ? "active" : ""}
-                        onClick={() => pageClick(i, currentPage)}>{i}</li>
-                )}
-                <li key="next"
-                    className={currentPage === numberOfPages ? "disable" : ""}
-                    onClick={() => pageClick(currentPage + 1, currentPage)}>&gt;</li>
-            </ul>
-        </div>;
-    } else {
-        return <div/>;
+            return <div>
+                <ul className="pagination">
+                    <li key="previous"
+                        className={currentPage === 1 ? "disable" : ""}
+                        accessKey=""
+                        onClick={() => this.pageClick(currentPage - 1)}>&lt;</li>
+                    {pages.map((i) =>
+                        <li key={i}
+                            className={i === currentPage ? "active" : ""}
+                            onClick={() => this.pageClick(i)}>{i}</li>
+                    )}
+                    <li key="next"
+                        className={currentPage === numberOfPages ? "disable" : ""}
+                        onClick={() => this.pageClick(currentPage + 1)}>&gt;</li>
+                </ul>
+            </div>;
+        } else {
+            return <div/>;
+        }
     }
 }
